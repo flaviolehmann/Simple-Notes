3107322275fc6f4fe370fb04add87063b2143ae6
Flávio Lehmann Júnior <flaviojunior0899@gmail.com>
2020-04-20 11:08:34 -0300

    feat back: exception handler and custom exceptions

--- backend/src/main/java/br/com/ledscolatina/backend/except/MainExceptionHandler.java
+++ backend/src/main/java/br/com/ledscolatina/backend/except/MainExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.ledscolatina.backend.except;
 
 import br.com.ledscolatina.backend.except.custom.CadernoNotFoundException;
+import br.com.ledscolatina.backend.except.custom.UsuarioNotFoundException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,10 +13,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
@@ -51,6 +48,21 @@ public class MainExceptionHandler extends ResponseEntityExceptionHandler {
                         400,
                         "Caderno inexistente no sistema.",
                         "caderno"
+                ),
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST,
+                request
+        );
+    }
+
+    @ExceptionHandler(value = { UsuarioNotFoundException.class })
+    protected ResponseEntity<Object> handleUsuarioNotFoundException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(
+                ex,
+                new ErrorResponseBody(
+                        400,
+                        "Usuário inexistente no sistema.",
+                        "usuario"
                 ),
                 new HttpHeaders(),
                 HttpStatus.BAD_REQUEST,
