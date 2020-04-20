6b7784009a0d9ef508dc57665dceb7f38833afe0
Flávio Lehmann Júnior <flaviojunior0899@gmail.com>
2020-04-20 11:09:11 -0300

    feat back: implementing security (jwt)

--- backend/src/main/java/br/com/ledscolatina/backend/repository/CadernoRepository.java
+++ backend/src/main/java/br/com/ledscolatina/backend/repository/CadernoRepository.java
@@ -1,7 +1,8 @@
 package br.com.ledscolatina.backend.repository;
 
-import br.com.ledscolatina.backend.domain.Caderno;
+import br.com.ledscolatina.backend.model.Caderno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CadernoRepository extends JpaRepository<Caderno, Long> {
+    Caderno findByUsuarioId(String usuarioId);
 }
--- backend/src/main/java/br/com/ledscolatina/backend/repository/NotaRepository.java
+++ backend/src/main/java/br/com/ledscolatina/backend/repository/NotaRepository.java
@@ -1,6 +1,6 @@
 package br.com.ledscolatina.backend.repository;
 
-import br.com.ledscolatina.backend.domain.Nota;
+import br.com.ledscolatina.backend.model.Nota;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotaRepository extends JpaRepository<Nota, Long> {
