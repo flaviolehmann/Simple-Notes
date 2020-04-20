395644b204ab4370505ac7e5ae4b5b194f66186d
Flávio Lehmann Júnior <flaviojunior0899@gmail.com>
2020-04-20 13:09:43 -0300

    refac back: rm useless imports

--- backend/src/main/java/br/com/ledscolatina/backend/controller/UsuarioController.java
+++ backend/src/main/java/br/com/ledscolatina/backend/controller/UsuarioController.java
@@ -2,20 +2,11 @@ package br.com.ledscolatina.backend.controller;
 
 import br.com.ledscolatina.backend.model.Usuario;
 import br.com.ledscolatina.backend.model.dto.*;
-import br.com.ledscolatina.backend.repository.UsuarioRepository;
 import br.com.ledscolatina.backend.service.UsuarioService;
 import br.com.ledscolatina.backend.util.DTO;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RestController
