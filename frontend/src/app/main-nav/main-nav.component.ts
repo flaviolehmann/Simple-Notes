e61232185135e83bfebc9081e0d3c066185e8204
Flávio Lehmann Júnior <flaviojunior0899@gmail.com>
2020-04-23 11:41:36 -0300

    feat front: adding navigation bar

---
+++ frontend/src/app/main-nav/main-nav.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { SegurancaService } from '../modules/seguranca/seguranca.service';
+
+@Component({
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.scss']
+})
+export class MainNavComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private segurancaService: SegurancaService
+    ) {}
+
+  onLogout(): void {
+    this.router.navigateByUrl('/login');
+    this.segurancaService.logout();
+  }
+}
